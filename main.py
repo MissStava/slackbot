4d06771e671f50761ce366db7d29af072d554571
Stephanie Hilton <stephanie.hilton@travisperkins.co.uk>
2019-11-27 16:00:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import slack
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+client = slack.WebClient(token=slack_token)
+
+client.chat_postMessage(
+    channel="slackertest",
+    text="Hello from your app! :tada:"
+)
