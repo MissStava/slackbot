6a6ad41773c63d5cdbd63a082ca815ea90eb60b6
Stephanie Hilton <stephanie.hilton@travisperkins.co.uk>
2019-11-28 16:37:48 +0000

    Switch to use rtm

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import os
-import slack
+from slack import RTMClient
 
-slack_token = os.environ["SLACK_API_TOKEN"]
-client = slack.WebClient(token=slack_token)
+@RTMClient.run_on(event="message")
+def say_hello(**payload):
+  data = payload['data']
+  print(data)
+  print("")
+  web_client = payload['web_client']
 
-client.chat_postMessage(
-    channel="slackertest",
-    text="Hello from your app! :tada:"
-)
+  if 'Hello' in data['text']:
+    channel_id = data['channel']
+    thread_ts = data['ts']
+    user = data['user']
+
+    web_client.chat_postMessage(
+      channel=channel_id,
+      text=f"Hi <@{user}>!",
+      thread_ts=thread_ts
+    )
+
+slack_token = os.environ["SLACK_TOKEN"]
+rtm_client = RTMClient(token=slack_token,proxy="http://127.0.0.1:3128")
+rtm_client.start()
